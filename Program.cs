42aae7eb3e3fa731cbf878a45910c608f5737f07
heisbrandonn <157356900+heisbrandonn@users.noreply.github.com>
2024-03-18 22:37:47 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using MusicPlayerApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Playlist playlist = new Playlist();
+
+        while (true)
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Add Song");
+            Console.WriteLine("2. Remove Song");
+            Console.WriteLine("3. Shuffle Playlist");
+            Console.WriteLine("4. Sort Playlist by Title");
+            Console.WriteLine("5. Sort Playlist by Artist");
+            Console.WriteLine("6. Play Playlist");
+            Console.WriteLine("7. Exit");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Enter song title:");
+                    string title = Console.ReadLine();
+                    Console.WriteLine("Enter artist:");
+                    string artist = Console.ReadLine();
+                    Console.WriteLine("Enter duration (hh:mm:ss):");
+                    TimeSpan duration = TimeSpan.Parse(Console.ReadLine());
+                    playlist.AddSong(new Song(title, artist, duration));
+                    break;
+                case "2":
+                    Console.WriteLine("Enter song title to remove:");
+                    string titleToRemove = Console.ReadLine();
+                    playlist.RemoveSong(titleToRemove);
+                    break;
+                case "3":
+                    playlist.Shuffle();
+                    Console.WriteLine("Playlist shuffled.");
+                    break;
+                case "4":
+                    playlist.SortByTitle();
+                    Console.WriteLine("Playlist sorted by title.");
+                    break;
+                case "5":
+                    playlist.SortByTitle();
+                    Console.WriteLine("Playlist sorted by Artist.");
+                    break;
+                case "6":
+                    Console.WriteLine("Playing playlist:");
+                    playlist.Play();
+                    break;
+                case "7":
+                    Console.WriteLine("Exiting...");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp
+{
+    internal class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public Song(string title, string artist, TimeSpan duration)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+        }
+    }
+
+    class Playlist
+    {
+        private List<Song> songs = new List<Song>();
+
+        public void AddSong(Song song)
+        {
+            songs.Add(song);
+        }
+
+        public void RemoveSong(string title)
+        {
+            Song songToRemove = songs.FirstOrDefault(s => s.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if (songToRemove != null)
+            {
+                songs.Remove(songToRemove);
+                Console.WriteLine($"Song '{title}' removed from the playlist.");
+            }
+            else
+            {
+                Console.WriteLine($"Song '{title}' not found in the playlist.");
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random rand = new Random();
+            songs = songs.OrderBy(x => rand.Next()).ToList();
+        }
+
+        public void SortByTitle()
+        {
+            songs = songs.OrderBy(x => x.Title).ToList();
+        }
+
+        public void Play()
+        {
+            foreach (var song in songs)
+            {
+                Console.WriteLine($"Playing: {song.Title} - {song.Artist} [{song.Duration}]");
+            }
+        }
+
+        public void SortByArtist()
+        {
+            songs = songs.OrderBy(x => x.Artist) .ToList();
+        }
+
+    }
+
+}
